d6e9d38889dea286c39a53bb4816a1be7a970f3c
João Victor <falejoaovictor@hotmail.com>
2021-10-04 11:05:12 -0300

    Criando a API

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Minha primeira API"
+
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    #tb_en = tb.translate(to="en")
+    polaridade = tb.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+app.run(debug=True)
