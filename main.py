4748a8a2fb3a2f1d64330bcaa80306a3045bd19b
João Victor <falejoaovictor@hotmail.com>
2021-10-04 14:58:23 -0300

    Adicionando Basic-Auth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
+from flask_basicauth import BasicAuth
 from flask import Flask, request, json, jsonify
 from textblob import TextBlob
 import pickle
@@ -7,12 +8,17 @@ colunas = ['tamanho', 'ano', 'garagem']
 modelo = pickle.load(open('modelo.sav','rb'))
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'joao'
+app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha primeira API"
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     #tb_en = tb.translate(to="en")
